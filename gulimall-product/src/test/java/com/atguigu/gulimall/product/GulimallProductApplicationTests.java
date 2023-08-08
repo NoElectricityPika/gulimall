7323d544e51274354c84de29ea944a637afc92f7
NoElectricityPika <okchizhang@gmail.com>
2023-08-08 00:09:20 -0400

    day5

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,5 +1,14 @@
 package com.atguigu.gulimall.product;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.common.auth.CredentialsProviderFactory;
+import com.aliyun.oss.common.auth.EnvironmentVariableCredentialsProvider;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.PutObjectResult;
+import com.aliyuncs.exceptions.ClientException;
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.service.BrandService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -9,6 +18,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -17,6 +29,9 @@ public class GulimallProductApplicationTests {
 
     @Autowired
     BrandService brandService;
+
+    @Autowired
+    OSSClient ossClient;
 
     @Test
     public void contextLoads() {
@@ -27,4 +42,12 @@ public class GulimallProductApplicationTests {
         }
     }
 
+
+    @Test
+    public void testUpload() throws ClientException, FileNotFoundException {
+        InputStream inputStream = new FileInputStream("C:\\Users\\OKC\\Documents\\1_java学习\\大型电商--谷粒商城\\1.分布式基础（全栈开发篇）\\分布式基础\\资源\\pics\\0d40c24b264aa511.jpg");
+        ossClient.putObject("gulimall-hellookc","0d40c24b264aa511.jpg",inputStream);
+        ossClient.shutdown();
+
+    }
 }
